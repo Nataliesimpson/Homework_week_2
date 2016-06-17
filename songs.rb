da51cee8787d75e6995d86745d01d21f9be63983
Natalie <natzsimpson@hotmail.com>
2016-06-17 11:54:33 +0100

    adds inital song tests

--- songs.rb
+++ songs.rb
@@ -1,1 +1,12 @@
 # name, category
+
+class Songs
+
+  attr_reader :song_name, :category
+
+  def initialize(song_name, category)
+    @song_name = song_name
+    @category = category
+  end
+  
+end    
--- specs/songs_specs.rb
+++ specs/songs_specs.rb
@@ -1,2 +1,25 @@
 # name, category
 # gets added to rooms 
+
+require('minitest/autorun')
+require_relative('../songs.rb')
+
+class TestSongs < MiniTest::Test
+
+  def setup
+    @songs = Songs.new("My Way", :oldschool)
+
+  end
+
+  def test_song_name
+    assert_equal("My Way", @songs.song_name)
+  end  
+
+  def test_song_category
+    assert_equal(:oldschool, @songs.category)
+  end  
+
+
+
+
+end    
