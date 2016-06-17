9462587e1c68aaf8564fc2f7764e2b481b97c02b
Natalie <natzsimpson@hotmail.com>
2016-06-17 12:00:04 +0100

    adds initial rooms tests

--- rooms.rb
+++ rooms.rb
@@ -1,1 +1,13 @@
 # name, capacity
+
+class Rooms
+
+  attr_reader :room_name, :capacity
+
+  def initialize(room_name, capacity)
+    @room_name = room_name
+    @capacity = capacity
+
+  end 
+  
+end     
--- specs/rooms_specs.rb
+++ specs/rooms_specs.rb
@@ -1,3 +1,22 @@
 # name, capacity
 # songs get added to rooms
 # guests get added to rooms
+require('minitest/autorun')
+require_relative('../rooms.rb')
+
+class TestRooms < MiniTest::Test
+
+  def setup
+    @rooms = Rooms.new("Gold_room", 3)
+
+  end 
+  
+def test_room_name
+  assert_equal("Gold_room", @rooms.room_name)
+end   
+
+def test_room_capacity
+  assert_equal(3, @rooms.capacity)
+end
+
+end    
